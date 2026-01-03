91d0e7f144862cf2b93d1608359afeffa6e87434
jz322 <joshuazimmerman33@gmail.com>
2026-01-03 22:15:14 +0000

    Final changes

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,7 +1,9 @@
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file
-
+from functions.get_files_info import get_files_info, schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
@@ -33,3 +35,20 @@ def call_function(function_call, verbose=False):
                 )
             ],
         )
+
+    args = dict(function_call.args) if function_call.args else {}
+
+    args["working_directory"] = "./calculator"
+
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
--- main.py
+++ main.py
@@ -5,40 +5,60 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from functions.call_function import available_functions
+from functions.call_function import call_function
+from config import MAX_ITERS
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+
 def main():
+    for i in range(MAX_ITERS):
+        response = client.models.generate_content(model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
 
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-    
+        function_results_list = []
+        if args.verbose:
+            print(f"User prompt: {messages}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+        if response.function_calls:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose=args.verbose)
+                if not function_call_result.parts:
+                    raise Exception("No parts list")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception("No response object")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception("No actual response")
+                function_results_list.append(function_call_result.parts[0])
 
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                
+            messages.append(types.Content(role="user", parts=function_results_list))
 
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
-
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-
-    if args.verbose:
-        print(f"User prompt: {messages}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
-    if response.function_calls:
-        for function_call in response.function_calls:
-            print(
-                f"Calling function: {function_call.name}({function_call.args})"
-            )
-    else:
-        print("Response:")
-        print(response.text)
-
+        else:
+            print("Response:")
+            print(response.text)
+            break
+        if i == 19:
+            print("AI could not solve.")
+            exit(1)
 
 
 if __name__ == "__main__":
